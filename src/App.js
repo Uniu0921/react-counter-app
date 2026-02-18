1b0952a8751a43fceca533d981493795c3d0382d
github user name <green0921tea@gmail.com>
2026-02-18 19:52:12 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  const increase = (num) => {
+    if (count + num <= 100) {
+      setCount(count + num);
+    }
+  };
+
+  const decrease = (num) => {
+    if (count - num >= 0) {
+      setCount(count - num);
+    }
+  };
+
+  const reset = () => {
+    setCount(0);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>カウンター</h1>
+      <h2>{count}</h2>
+
+      <button onClick={() => increase(1)}>+1</button>
+      <button onClick={() => decrease(1)}>-1</button>
+
+      <br /><br />
+
+      <button onClick={() => increase(10)}>+10</button>
+      <button onClick={() => decrease(10)}>-10</button>
+
+      <br /><br />
+
+      <button onClick={reset}>リセット</button>
     </div>
   );
 }
